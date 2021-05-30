76017198aa30976db965018e31b6bef0d2c39b07
Krit Bannachaisirisuk <gmkipladen@gmail.com>
2021-05-30 11:05:53 +0700

    Add reference and best pratice to refresh when change network

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # ethereum-demo
-To learn on basic ethereum development
+To learn on basic ethereum development.
 
 # Prerequisite
 - Nodejs version 15.9.0
+
+# Reference
+- [The Complete Guide to Full Stack Ethereum Development by Nader Dabit](https://www.freecodecamp.org/news/full-stack-ethereum-development/)
--- react-dapp/src/App.tsx
+++ react-dapp/src/App.tsx
@@ -11,6 +11,17 @@ function App() {
   // store greeting in local state
   const [greeting, setGreetingValue] = useState<string>()
 
+  // https://docs.ethers.io/v5/concepts/best-practices/
+  const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+  provider.on("network", (newNetwork, oldNetwork) => {
+      // When a Provider makes its initial connection, it emits a "network"
+      // event with a null oldNetwork along with the newNetwork. So, if the
+      // oldNetwork exists, it represents a changing network
+      if (oldNetwork) {
+          window.location.reload();
+      }
+  });
+
   // request access to the user's MetaMask account
   async function requestAccount() {
     await window.ethereum.request({ method: 'eth_requestAccounts' });
